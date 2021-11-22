8e1393cd1f2ef184462934346811fd8fa8b82596
Fan Shi <shifan@jsk.imi.i.u-tokyo.ac.jp>
2021-11-23 01:37:14 +0900

    fix copy path.

--- utils/object_detection_training_utils/docker/object_detection/docker_train.sh
+++ utils/object_detection_training_utils/docker/object_detection/docker_train.sh
@@ -6,4 +6,5 @@ bash generate_record.sh
 python3 model_main-fine-tune.py --model_dir=models/my_ssd --pipeline_config_path=models/my_ssd/pipeline.config --num_train_steps=2000
 cd ~/Tensorflow/demo/models
 bash convert.sh
-cp ~/Tensorflow/demo/models ~/Tensorflow/demo/models_test -r
+cd ~/Tensorflow/demo
+cp models models_test -r
