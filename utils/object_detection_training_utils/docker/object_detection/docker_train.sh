02d70abb77eb3066e7e97621a178d953487716db
Fan Shi <shifan@jsk.imi.i.u-tokyo.ac.jp>
2021-11-22 21:08:49 +0900

    fix bug to convert to tpu model.

--- utils/object_detection_training_utils/docker/object_detection/docker_train.sh
+++ utils/object_detection_training_utils/docker/object_detection/docker_train.sh
@@ -4,3 +4,5 @@ conda activate aero_train
 cd ~/Tensorflow/demo/
 bash generate_record.sh
 python3 model_main-fine-tune.py --model_dir=models/my_ssd --pipeline_config_path=models/my_ssd/pipeline.config --num_train_steps=2000
+cd ~/Tensorflow/demo/models
+bash convert.sh
