9d71d7b6e88c122acb8502791d7245568a3fdc2f
Fan Shi <shifan@jsk.imi.i.u-tokyo.ac.jp>
2021-11-27 00:15:09 +0900

    add iteration times arg.

--- utils/object_detection_training_utils/docker/object_detection/docker_train.sh
+++ utils/object_detection_training_utils/docker/object_detection/docker_train.sh
@@ -1,10 +1,13 @@
+ITERATION_TIMES=$1
+
 source /opt/anaconda3/bin/activate
 conda activate aero_train
 
 cd ~/Tensorflow/demo/
 bash generate_record.sh
-python3 model_main-fine-tune.py --model_dir=models/my_ssd --pipeline_config_path=models/my_ssd/pipeline.config --num_train_steps=2000
+python3 model_main-fine-tune.py --model_dir=models/my_ssd --pipeline_config_path=models/my_ssd/pipeline.config --num_train_steps=$ITERATION_TIMES
 cd ~/Tensorflow/demo/models
+sed -i 's/2000/$ITERATION_TIMES/g' convert.sh
 bash convert.sh
 cd ~/Tensorflow/demo
 cp models models_test -r
--- utils/object_detection_training_utils/docker/train_object_detection_dlbox.sh
+++ utils/object_detection_training_utils/docker/train_object_detection_dlbox.sh
@@ -17,11 +17,13 @@ DATASET_DIR=$(realpath $1); shift 1;
 DATASET_NAME=$(basename $DATASET_DIR)
 echo $DATASET_DIR
 
+ITERATION_TIMES=3000
+
 DATE=$(date +"%Y%m%d-%H%M%S")
 
 ## DLBOX use personal user id currently
 # DLBOX_IP=shifan@dlbox14.jsk.imi.i.u-tokyo.ac.jp
-DLBOX_IP=shifan@dlbox8.jsk.imi.i.u-tokyo.ac.jp
+DLBOX_IP=shifan@dlbox6.jsk.imi.i.u-tokyo.ac.jp
 
 set -x
 scp -q -r $DATASET_DIR $DLBOX_IP:~/$DATE-$DATASET_NAME
@@ -37,6 +39,6 @@ cat <<EOF | ssh -t $DLBOX_IP
     docker build -t object_detection_base object_detection_docker/object_detection_base
     docker build -t object_detection object_detection_docker/object_detection
     wget https://raw.githubusercontent.com/fanshi14/multiple-grasping-pose-learning/add_dockerfile/utils/object_detection_training_utils/docker/object_detection/run_detection.sh -O run_detection.sh
-    bash run_detection.sh $DATE-$DATASET_NAME
+    bash run_detection.sh $DATE-$DATASET_NAME $ITERATION_TIMES
 EOF
 scp -r $DLBOX_IP:~/$DATE-$DATASET_NAME/learn $DATASET_DIR
