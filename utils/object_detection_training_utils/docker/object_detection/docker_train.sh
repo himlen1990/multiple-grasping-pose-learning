e0109ec6f92fe0b0f681792e6a58542e7e4feba9
Fan Shi <shifan@jsk.imi.i.u-tokyo.ac.jp>
2021-11-27 00:43:46 +0900

    fix converter iteration variable passing.

--- utils/object_detection_training_utils/docker/object_detection/docker_train.sh
+++ utils/object_detection_training_utils/docker/object_detection/docker_train.sh
@@ -7,7 +7,6 @@ cd ~/Tensorflow/demo/
 bash generate_record.sh
 python3 model_main-fine-tune.py --model_dir=models/my_ssd --pipeline_config_path=models/my_ssd/pipeline.config --num_train_steps=$ITERATION_TIMES
 cd ~/Tensorflow/demo/models
-sed -i 's/2000/$ITERATION_TIMES/g' convert.sh
-bash convert.sh
+bash convert.sh $ITERATION_TIMES
 cd ~/Tensorflow/demo
 cp models models_test -r
